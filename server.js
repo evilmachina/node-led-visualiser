af9a60d1dd42bcb37d2034699b4eeed1e7e25bec
Marcus Olsson <marcus.h.olsson@gmail.com>
2013-09-24 22:07:01 +0200

    added suport for arduino

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var LightStrips = require('./LPD8806').LightStrips;
 var numberOfLEDs = 64;
 var lights = new LightStrips('/dev/spidev0.0', numberOfLEDs);
 
+var xbeeSerialPort = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A800csie-if00-port0';
+
+var serialport = require('serialport');
+var serialPort = new serialport.SerialPort(xbeeSerialPort,
+{//Listening on the serial port for data coming from Arduino over USB
+	baudRate: 57600,
+	parser: serialport.parsers.readline('\n')
+});
+
 var off = function(){
    lights.off();
 };
@@ -30,6 +39,12 @@ var showAmplitude = function(){
 	if(data.rgb){
 		rgb = data.rgb;
 	}
+	try{
+			serialPort.write(new Buffer([0x04, rgb[0], rgb[1], rgb[2],0x10]));
+	}catch(e){
+		console.log(e);
+	}
+
 	lightsOn(data.percentage, rgb);
 };
 
